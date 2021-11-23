11756e35b7c4a0f081af16d0ec8c610cde0beee2
Christian Colson <christiancolson91@gmail.com>
2021-11-23 14:35:49 -0600

    initial commit - client side html/js files and css

---
+++ client/index.js
@@ -0,0 +1,31 @@
+const toggle = document.getElementById('toggle')
+
+const modalSignup = document.getElementById('modal-signup')
+const signUp = document.getElementById('open-signup')
+const closeSignup = document.getElementById('close-signup')
+
+const modalSignin = document.getElementById('modal-signin')
+const signIn = document.getElementById('open-signin')
+const closeSignin = document.getElementById('close-signin')
+
+// Toggle Navigation
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'))
+
+// Show modal sign up
+signUp.addEventListener('click', () => modalSignup.classList.add('show-modal-signup'))
+
+// Hide modal sign up
+closeSignup.addEventListener('click', () => modalSignup.classList.remove('show-modal-signup'))
+
+// Hide modal sign up on outside click
+window.addEventListener('click', e => e.target == modalSignup ? modalSignup.classList.remove('show-modal-signup') : false)
+
+// Show modal sign in
+signIn.addEventListener('click', () => modalSignin.classList.add('show-modal-signin'))
+
+// Hide modal sign in
+closeSignin.addEventListener('click', () => modalSignin.classList.remove('show-modal-signin'))
+
+// Hide modal sign in on outside click
+
+window.addEventListener('click', e => e.target == modalSignin ? modalSignin.classList.remove('show-modal-signin') : false)

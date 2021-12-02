49faecd29cb14ffd0ebb858c9c1d4d8a9adee89c
Christian Colson <christiancolson91@gmail.com>
2021-12-02 00:23:45 -0600

    updates to design and everything working

---
+++ client/send-offers.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById('toggle')
+// Toggle Navigation
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'))
+
+let sendEmailBtn = document.getElementById('send-email-btn')
